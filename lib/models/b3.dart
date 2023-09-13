b4b3531fcc31c7945b96c93239915cdbe3ea7230
Henrique Sebastiao <rchsan8@gmail.com>
2023-09-13 14:02:25 -0300

    updating b3 json and component

--- lib/models/b3.dart
+++ lib/models/b3.dart
@@ -1,22 +1,47 @@
+import 'dart:convert';
+
 import 'package:equatable/equatable.dart';
 import 'package:invest_manager/components/b3.dart';
 
-class Company extends Equatable {
+class CompanyTicker extends Equatable {
+  const CompanyTicker({
+    required this.b3code,
+    this.isinCode = "",
+  });
 
+  final String b3code;
+  final String isinCode;
+
+  factory CompanyTicker.fromJson(Map<String, dynamic> json) {
+    return CompanyTicker(
+      b3code: json["code"],
+      isinCode: json["isin"],
+    );
+  }
+
+  @override
+  List<Object> get props => [
+        b3code,
+        isinCode,
+      ];
+}
+
+class Company extends Equatable {
   const Company({
     required this.cvmCode,
     required this.name,
     required this.issuingCompany,
     required this.tradingName,
     required this.taxId,
-    required this.dateListing,
-    this.marketIndicator = "",
-    this.typeBDR = "",
+    required this.listedSince,
     this.status = "",
     this.segment = "",
-    this.segmentEng = "",
-    this.type = "",
-    this.market = "",
+    this.tickers = const [],
+    this.activity = "",
+    this.industryCassification = "",
+    this.institutionCommon = "",
+    this.institutionPreferred = "",
+    this.website,
   });
 
   final String cvmCode;
@@ -24,14 +49,15 @@ class Company extends Equatable {
   final String name;
   final String tradingName;
   final String taxId;
-  final String marketIndicator;
-  final String typeBDR;
-  final DateTime dateListing;
   final String status;
   final String segment;
-  final String segmentEng;
-  final String type;
-  final String market;
+  final DateTime listedSince;
+  final List<CompanyTicker> tickers;
+  final String industryCassification;
+  final String activity;
+  final String institutionCommon;
+  final String institutionPreferred;
+  final Uri? website;
 
   factory Company.fromJson(Map<String, dynamic> json) {
     return Company(
@@ -40,32 +66,36 @@ class Company extends Equatable {
       issuingCompany: json['issuingCompany'],
       name: json['companyName'],
       tradingName: json['tradingName'],
-      marketIndicator: json['marketIndicator'],
-      typeBDR: json['typeBDR'],
-      dateListing: dateFormat.parse(json['dateListing']),
       status: json['status'],
       segment: json['segment'],
-      segmentEng: json['segmentEng'],
-      type: json['type'],
-      market: json['market'],
+      listedSince: dateFormat.parse(json['dateListing']),
+      tickers: (jsonDecode(json["codes"]) as List<dynamic>)
+          .map((e) => e as Map<String, dynamic>)
+          .map(CompanyTicker.fromJson)
+          .toList(),
+      industryCassification: json["industryClassification"],
+      institutionCommon: json['institutionCommon'],
+      activity: json['activity'],
+      institutionPreferred: json['institutionPreferred'],
+      website: Uri.tryParse(json['website']),
     );
   }
 
   @override
-  // TODO: implement props
   List<Object?> get props => [
         cvmCode,
         issuingCompany,
         name,
         tradingName,
         taxId,
-        marketIndicator,
-        typeBDR,
-        dateListing,
         status,
         segment,
-        segmentEng,
-        type,
-        market,
+        listedSince,
+        tickers,
+        institutionPreferred,
+        institutionCommon,
+        industryCassification,
+        activity,
+        website,
       ];
 }
